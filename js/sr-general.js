15fbb43589a7d066712bb2149126980d1b627fee
SrGalleto <romankr1997@gmail.com>
2023-02-11 13:31:41 +0100

    añadido scroll reveal de prueba a index

---
+++ js/sr-general.js
@@ -0,0 +1,49 @@
+window.sr = ScrollReveal();
+
+/* --- TOP --- */
+sr.reveal('.sr-top-1',{
+    duration: 3000,
+    interval: 250,
+    origin: 'bottom',
+    distance: '250px'
+})
+
+sr.reveal('.sr-top-2',{
+    duration: 1500,
+    interval: 150,
+    origin: 'bottom',
+    distance: '200px'
+})
+
+sr.reveal('.sr-top-3',{
+    duration: 1500,
+    interval: 150,
+    origin: 'bottom',
+    distance: '150px'
+})
+
+sr.reveal('.sr-top-4',{
+    duration: 1500,
+    interval: 150,
+    origin: 'bottom',
+    distance: '100px'
+})
+
+/* --- BOTTOM --- */
+
+
+/* --- RIGHT --- */
+sr.reveal('.sr-right-4',{
+    duration: 1500,
+    interval: 150,
+    origin: 'left',
+    distance: '100px'
+})
+
+/* --- LEFT --- */
+sr.reveal('.sr-left-4',{
+    duration: 1500,
+    interval: 150,
+    origin: 'right',
+    distance: '100px'
+})
